4e3e4db285508ed07b47bc5d29b3f452f3d3cf97
Diny-zhe <15147967473@163.com>
2018-04-25 00:20:06 +0800

    实现两重循环日记卡

--- data/data.js
+++ data/data.js
@@ -1,8 +1,8 @@
 
 var dairyList=[
   {
-    title:"很开心efhjbfjhbjfhberjfhbejrbjherfbjherfbjehrfbjhebfjerhbfhberjhbfjrehf！",
-    content:"吉萨本科插班生开场白酷暑的出口标识的客户保持可谁都不吃亏还是本地缓存白色的本场比赛计划的补偿金",
+    title:"1",
+    content:"吉萨本科插班生开场白酷暑的出口标识的客户保持可谁都不吃白色的本场比赛计划的补偿金",
     id:1,
     emotion:1,
     data:"2018-04-11"
@@ -16,67 +16,242 @@ var dairyList=[
   },
   {
     title: "3",
-    content: "sjkffjdbjhf",
+    content: "kjsddnkjdbvkdf",
     id: 3,
-    emotion: 3,
-    data: "2018-04-09"
+    emotion: 2,
+    data: "2018-04-11"
   },
   {
     title: "4",
     content: "kjsddnkjdbvkdf",
     id: 4,
-    emotion: 4,
-    data: "2018-04-06"
+    emotion: 2,
+    data: "2018-04-11"
   },
   {
     title: "5",
     content: "kjsddnkjdbvkdf",
     id: 5,
-    emotion: 5,
-    data: "2018-04-06"
+    emotion: 2,
+    data: "2018-04-11"
   },
   {
     title: "6",
     content: "kjsddnkjdbvkdf",
     id: 6,
-    emotion: 6,
-    data: "2018-04-03"
+    emotion: 2,
+    data: "2018-04-11"
   },
   {
     title: "7",
-    content: "kjsddnkjdbvkdf",
+    content: "sjkffjdbjhf",
     id: 7,
+    emotion: 3,
+    data: "2018-04-09"
+  },
+  {
+    title: "8",
+    content: "sjkffjdbjhf",
+    id: 8,
+    emotion: 3,
+    data: "2018-04-09"
+  },
+  {
+    title: "9",
+    content: "sjkffjdbjhf",
+    id: 9,
+    emotion: 3,
+    data: "2018-04-09"
+  },
+  {
+    title: "10",
+    content: "sjkffjdbjhf",
+    id: 10,
+    emotion: 3,
+    data: "2018-04-09"
+  },
+  {
+    title: "11",
+    content: "sjkffjdbjhf",
+    id: 11,
+    emotion: 3,
+    data: "2018-04-09"
+  },
+  {
+    title: "12",
+    content: "sjkffjdbjhf",
+    id: 12,
+    emotion: 3,
+    data: "2018-04-09"
+  },
+  {
+    title: "13",
+    content: "kjsddnkjdbvkdf",
+    id: 13,
+    emotion: 4,
+    data: "2018-04-06"
+  },
+  {
+    title: "14",
+    content: "kjsddnkjdbvkdf",
+    id: 14,
+    emotion: 5,
+    data: "2018-04-06"
+  },
+  {
+    title: "15",
+    content: "kjsddnkjdbvkdf",
+    id: 15,
+    emotion: 5,
+    data: "2018-04-06"
+  },
+  {
+    title: "16",
+    content: "kjsddnkjdbvkdf",
+    id: 16,
+    emotion: 5,
+    data: "2018-04-06"
+  },
+  {
+    title: "17",
+    content: "kjsddnkjdbvkdf",
+    id: 17,
+    emotion: 5,
+    data: "2018-04-06"
+  },
+  {
+    title: "18",
+    content: "kjsddnkjdbvkdf",
+    id: 18,
+    emotion: 5,
+    data: "2018-04-06"
+  },
+  {
+    title: "19",
+    content: "kjsddnkjdbvkdf",
+    id: 19,
     emotion: 6,
     data: "2018-04-03"
   },
   {
-    title: "8",
+    title: "20",
     content: "kjsddnkjdbvkdf",
-    id: 8,
+    id: 20,
+    emotion: 6,
+    data: "2018-04-03"
+  },
+  {
+    title: "21",
+    content: "kjsddnkjdbvkdf",
+    id: 21,
+    emotion: 6,
+    data: "2018-04-03"
+  },
+  {
+    title: "22",
+    content: "kjsddnkjdbvkdf",
+    id: 22,
+    emotion: 6,
+    data: "2018-04-03"
+  },
+  {
+    title: "23",
+    content: "kjsddnkjdbvkdf",
+    id: 23,
+    emotion: 6,
+    data: "2018-04-03"
+  },
+  {
+    title: "24",
+    content: "kjsddnkjdbvkdf",
+    id: 24,
     emotion: 6,
     data: "2018-04-03"
   },
     {
-    title: "9",
+    title: "25",
     content: "kjsddnkjdbvkdf",
-    id: 9,
+    id: 25,
     emotion: 6,
     data: "2018-04-01"
   },
     {
-      title: "10",
+      title: "26",
       content: "kjsddnkjdbvkdf",
-      id: 6,
+      id: 26,
       emotion: 6,
       data: "2018-04-01"
     },
     {
-      title: "11",
+      title: "27",
       content: "kjsddnkjdbvkdf",
-      id: 11,
+      id: 27,
       emotion: 6,
       data: "2018-04-01"
-    }
+    },
+    {
+      title: "28",
+      content: "kjsddnkjdbvkdf",
+      id: 28,
+      emotion: 6,
+      data: "2018-04-01"
+    },
+    {
+      title: "29",
+      content: "kjsddnkjdbvkdf",
+      id: 29,
+      emotion: 6,
+      data: "2018-04-01"
+    },
+    {
+      title: "30",
+      content: "kjsddnkjdbvkdf",
+      id: 30,
+      emotion: 6,
+      data: "2018-04-01"
+    },
+    {
+      title: "31",
+      content: "kjsddnkjdbvkdf",
+      id: 31,
+      emotion: 6,
+      data: "2018-03-22"
+    },
+    {
+      title: "32",
+      content: "kjsddnkjdbvkdf",
+      id: 32,
+      emotion: 6,
+      data: "2018-03-22"
+    },
+    {
+      title: "33",
+      content: "kjsddnkjdbvkdf",
+      id: 33,
+      emotion: 6,
+      data: "2018-03-22"
+    },
+    {
+      title: "34",
+      content: "kjsddnkjdbvkdf",
+      id: 34,
+      emotion: 6,
+      data: "2018-03-22"
+    },
+    {
+      title: "35",
+      content: "kjsddnkjdbvkdf",
+      id: 35,
+      emotion: 6,
+      data: "2018-03-22"
+    },
+    {
+      title: "36",
+      content: "kjsddnkjdbvkdf",
+      id: 36,
+      emotion: 6,
+      data: "2018-03-22"
+    },
 ]
 var arr = new Array(6);   //表格有10行  
 for (var i = 0; i < arr.length; i++) {
