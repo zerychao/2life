6e2d155757df8f556aa49a67450b387c03d23c5e
zerychao <zzy_0104@126.com>
2018-04-25 09:08:23 +0800

    修复代码

--- pages/main/main.js
+++ pages/main/main.js
@@ -17,11 +17,7 @@ Page({
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true,
-<<<<<<< HEAD
         diaryPartList: dummyData.dairyList
-=======
-        diaryList:dataObj.diaryList
->>>>>>> 3ce65311f2e1d6a387c99fc8574316efd36fd4ec
       })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -30,11 +26,7 @@ Page({
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true,
-<<<<<<< HEAD
           diaryPartList: dummyData.dairyList
-=======
-          diaryList: dataObj.diaryList
->>>>>>> 3ce65311f2e1d6a387c99fc8574316efd36fd4ec
         })
       }
     } else {
@@ -45,11 +37,7 @@ Page({
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true,
-<<<<<<< HEAD
             diaryPartList: dummyData.dairyList
-=======
-            diaryList: dataObj.diaryList
->>>>>>> 3ce65311f2e1d6a387c99fc8574316efd36fd4ec
           })
         }
       })
