293abedd206ac17a193f95663370e6daa0afe4ba
zerychao <zzy_0104@126.com>
2018-04-26 01:04:30 +0800

    规范函数命名

--- utils/util.js
+++ utils/util.js
@@ -21,45 +21,50 @@ module.exports = {
     return n[1] ? n : '0' + n
   },
 
-  parseDiarydata: diaryList => {
-    // 遍历对象计算共有多少相同的日期
-    var n = 1;
-    var date = diaryList[0].date;
-    for (var l = 1; l < diaryList.length; l++) {
-      if (date != diaryList[l].date) {
-        date = diaryList[l].date;
-        n++;
+  parseDiaryData: {
+
+    splitByDate: diaryList => {
+      // 遍历对象计算共有多少相同的日期
+      var n = 1;
+      var date = diaryList[0].date;
+      for (var l = 1; l < diaryList.length; l++) {
+        if (date != diaryList[l].date) {
+          date = diaryList[l].date;
+          n++;
+        }
       }
-    }
-    var arr = new Array(n);
-    for (var i = 0; i < arr.length; i++) {
-      arr[i] = new Array();
-    }
-    arr[0][0] = diaryList[0];
-    var count = 0;
-    for (var i = 1, j = 1; i < diaryList.length; i++ , j++) {
-      if (diaryList[j].date == diaryList[j - 1].date) {
-        arr[count][j] = diaryList[i];
+      var arr = new Array(n);
+      for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array();
       }
-      else {
-        count++;
-        j = 0;
-        arr[count][j] = diaryList[i];
+      arr[0][0] = diaryList[0];
+      var count = 0;
+      for (var i = 1, j = 1; i < diaryList.length; i++ , j++) {
+        if (diaryList[j].date == diaryList[j - 1].date) {
+          arr[count][j] = diaryList[i];
+        }
+        else {
+          count++;
+          j = 0;
+          arr[count][j] = diaryList[i];
+        }
       }
+      return arr;
+    },
+
+    // 实现日期只有日的方法
+    dateToDayWeekday: diaryList => {
+      var arr2 = new Array(diaryList.length);
+      for (var i = 0; i < diaryList.length; i++) {
+        arr2[i] = diaryList[i];
+      }
+      for(var j=0;j<arr2.length;j++){
+        var date_new = arr2[j].date.split(/-/);
+        arr2[j].date = date_new[2];
+      }
+      return arr2;
     }
-    return arr;
-  },
-// 实现日期只有日的方法
-  dateToWeek: diaryList => {
-    var arr2 = new Array(diaryList.length);
-    for (var i = 0; i < diaryList.length; i++) {
-      arr2[i] = diaryList[i];
-    }
-    for(var j=0;j<arr2.length;j++){
-      var date_new = arr2[j].date.split(/-/);
-      arr2[j].date = date_new[2];
-    }
-    return arr2;
+
   }
   
 }
